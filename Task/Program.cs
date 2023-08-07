e1e388ee5d15c482f77be138ecba1f2746ff9fd2
Contempl <the_mega_gamexc76@mail.ru>
2023-08-07 18:41:44 +0300

    Iteration1. Refactoring

---
+++ Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Taski
+{
+    internal class Program
+    {
+        private static readonly DateTime Now = DateTime.Now;
+
+        private static async Task Main(string[] args)
+        {
+            if (args.Length < 1 || args.Length > 2)
+            {
+                Console.WriteLine("Wrong input, debil");
+                return;
+            }
+            var resource = args[0];
+            var defaultName = $"{args[0].Replace("https://", string.Empty)}_request_{Now}.log";
+            var fileName = args.Length == 2 ? args[1] : defaultName;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                using var client = new HttpClient();
+                var output = await client.GetStringAsync(resource);
+                await File.WriteAllTextAsync(fileName, output);
+                Console.WriteLine("Status code: 200");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Electric. We are electric... and your request failed {ex.Message}");
+            }
+            finally
+            {
+                stopwatch.Stop();
+                Console.WriteLine($"Request took {stopwatch.ElapsedMilliseconds} ms");
+            }
+        }
+    }
+}
