d6655af5c00bb18d46b12cf9696fc5a06522ad47
Mykhailo Konontsev <fater181@gmail.com>
2023-08-17 20:18:53 +0300

    Task project added

---
+++ Task.Unit.Tests/CommandLineArgsTest.cs
@@ -0,0 +1,7 @@
+namespace Task.Unit.Tests;
+
+public class CommandLineArgsTest
+{
+    // Watch videos about unit testing
+    // Fully cover CommandLineArgs class
+}
---
+++ Task/CommandLineArgs.cs
@@ -0,0 +1,21 @@
+﻿namespace Task;
+
+public class CommandLineArgs
+{
+    public string FilePath { get; private set; }
+    public string OutputMode { get; private set; }
+
+    public CommandLineArgs(string[] args)
+    {
+        if (args.Length > 2)
+            throw new ArgumentException();
+
+        if (args.Length == 1)
+        {
+            throw new ArgumentException();
+        }
+        
+        FilePath = args[0];
+        OutputMode = args[1];
+    }
+}
